338e269cd9492067e2fd04add87897dff977bddb
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2023-02-07 21:45:11 +0200

    Add feature to automatically search and process csv files in current directory and illegal character fix

--- calculator.py
+++ calculator.py
@@ -1,23 +1,32 @@
 import csv
 from datetime import datetime
+import os
+import re
 
-with open('data.csv', 'r') as file:
-    reader = csv.DictReader(file)
-    data = list(reader)
+filesList = os.listdir() # seems like the most appropriate scope
 
-unfiltered_data = []
-for row in data:
-    unfiltered_data.append(row)
 
-spent = 0
-for row in unfiltered_data:
-    trans_value = float (row["Transaction Value"])
-    if (row["Transaction Type"] == "CSC Fare Product checkout") or (row["Transaction Type"] == "CSC Fare Product checkin"):
-        spent -= trans_value
-    elif (row["Transaction Type"] == "CSC tPurse Product Reload") and (trans_value == 14):
-        spent -= trans_value
-        
-        
-print("Amount Spent on Transport from ", data[0]["Transaction Date"], " until ", data[-1]["Transaction Date"], ": R", spent)
+def calculate_costs(filename = 'data.csv'):
+    with open(filename, 'r') as file:
+        reader = csv.DictReader(file)
+        data = list(reader)
 
- 
+
+    spent = 0
+    for row in data:
+        trans_value = float (row["Transaction Value"])
+        if (row["Transaction Type"] == "CSC Fare Product checkout") or (row["Transaction Type"] == "CSC Fare Product checkin"):
+            spent -= trans_value
+        elif (row["Transaction Type"] == "CSC tPurse Product Reload") and (trans_value == 14): #discount is always R14
+            spent -= trans_value
+            
+            
+    print("Calculated using file:",filename,"\nTimeline:", data[-1]["TransactionÂ\xa0Date"], " - ", data[0]["TransactionÂ\xa0Date"], "\nSpent: R", spent, "\n")
+
+    return spent
+
+if __name__ == "__main__":
+
+    for file in filesList:
+        if bool(re.search(r"^[0-9A-Za-z)\s$(+*'\"_\-]+\.csv$", file)): #any file with ".csv" at the end, and at least 1 character as it's name. Using '[.]' isn't working, so missing some legal special characters
+            calculate_costs(file)
